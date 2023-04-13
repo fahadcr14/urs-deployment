c7ff58f0a35a1ca147e486e2d3f848e56f13c60a
fahadcr14 <124671002+fahadcr14@users.noreply.github.com>
2023-04-13 09:02:55 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 python3.9 -m pip install django
-python3.9 -m pip install PyPI
+python3.9 -m pip install psycopg
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
